0bdd08e8d680504cb75fa326474935a8f84dd6c6
sergoops <sergoopsmoskal@gmail.com>
2023-09-23 09:57:54 +0300

    picker: Update to new vendor/aospa shasum

--- picker.sh
+++ picker.sh
@@ -62,7 +62,7 @@ git restore --staged .
 git restore .
 if [ $2 != "r" ]; then
     git fetch $vnd_aospa --depth 2
-    git cherry-pick 5a27c1529322f07bd495fab636669005bef61177 $CHERRYPICK_FLAGS
+    git cherry-pick 367681486b165de2f0c2cd4417aa3f91761b91d8 $CHERRYPICK_FLAGS
 fi
 
 # vnd_ggl_pxl
