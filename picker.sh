91f057cd7198b610f7d851c4d180f9cf315f3648
sergoops <sergoopsmoskal@gmail.com>
2024-10-30 23:02:42 +0300

    picker: Update

--- picker.sh
+++ picker.sh
@@ -41,7 +41,7 @@ git restore --staged .
 git restore .
 if [ $2 != "r" ]; then
     git fetch $fw_av --depth 3
-    git cherry-pick 872e6b8ba168245420d4e63bedd2357bbf40b7ca^..5c371cf92245d2a6f9958a8621a142dfb3ff960c $CHERRYPICK_FLAGS
+    git cherry-pick ed9b1f8365f9ea62e3b40e997688c104cd0da91e^..b8e8d3779f5bc28a323a33f739d8e1cb7da2366e $CHERRYPICK_FLAGS
 fi
 
 # system_core
