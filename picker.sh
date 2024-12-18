76b57a607035272fc8fac05b504bfce6e13c504f
sergoops <sergoopsmoskal@gmail.com>
2024-12-18 23:58:29 +0300

    picker: Update

--- picker.sh
+++ picker.sh
@@ -14,14 +14,24 @@ CHERRYPICK_FLAGS="--no-commit"
 
 # Repositories
 build_soong="git@github.com:LineageOS-oops/android_build_soong.git"
+fw_av="git@github.com:LineageOS-oops/android_frameworks_av.git"
 
-# fw_base
+# build_soong
 cd $SOURCE_ROOT/build/soong
 git restore --staged .
 git restore .
 if [ $2 != "r" ]; then
-    git fetch $fw_base --depth 2
+    git fetch $build_soong --depth 2
     git cherry-pick 1c92461e564df35747ac99aabaaea18cc37bf7a3 $CHERRYPICK_FLAGS
+fi
+
+# fw_av
+cd $SOURCE_ROOT/frameworks/av
+git restore --staged .
+git restore .
+if [ $2 != "r" ]; then
+    git fetch $fw_av --depth 3
+    git cherry-pick f453e2d4925b282e7c33ce3b878d8938af4b8fc3^..24db63741bd6ac80b704bca023262a9631a2962d $CHERRYPICK_FLAGS
 fi
 
 exit 0
